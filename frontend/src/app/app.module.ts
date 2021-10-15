f7d574ec86a355ef576b31c7d018ed4c4af70277
leoramos182 <leo.ramos182@hotmail.com>
2021-10-15 14:15:02 -0300

    criado sidenav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,23 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderComponent } from './components/template/header/header.component'
-import {MatIconModule} from '@angular/material/icon';
-import { FooterComponent } from './components/template/footer/footer.component'
+import { MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavbarComponent } from './components/template/sidenav/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
